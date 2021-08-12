7ea5bb42a73523f2f9e037715d35062095c5afc6
Sofia <sraissiguier@gmail.com>
2021-08-12 13:41:47 -0300

    Create Dog Model

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.JSONB,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.JSONB,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.JSONB
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
