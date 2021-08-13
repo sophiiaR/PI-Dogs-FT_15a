cd494fd999dd9456e5a04b70e1ecbfca44150b95
Sofia <sraissiguier@gmail.com>
2021-08-13 19:42:13 -0300

    Fix typo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWhitDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
-export const store = createStore(rootReducer, composeWhitDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
