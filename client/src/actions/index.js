a80bd1ea435443b3708e4bd17dea817fdc01f713
Sofia <sraissiguier@gmail.com>
2021-08-14 20:59:01 -0300

    Create redux actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+export function getDogs() {
+    return async function(dispatch) {
+        try {
+            const dogs = await axios.get('http://localhost:3001/dogs');
+            return dispatch({ 
+                type: 'GET_ALL_DOGS', 
+                payload: dogs.data 
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+export function filterCreated(payload) {
+    console.log(payload)
+    return ({
+        type: 'FILTER_CREATED',
+        payload
+    })
+}
+
+export function orderDogsByName(payload) {
+    return {
+        type: 'ORDER_BY_NAME',
+        payload
+    }
+}
+
+export function getDogsByName(payload) {
+    return async function(dispatch) {
+        try {
+            const dogsByName = await axios.get('http://localhost:3001/dogs?name=' + payload);
+            return dispatch ({
+                type: 'GET_DOGS_BY_NAME',
+                payload: dogsByName.data //lo que devuelve la ruta /dogs luego de haber buscado por el query name
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export function getTemperaments() {
+    return async function (dispatch) {
+        try {
+            const temp = await axios.get('http://localhost:3001/temperament');
+            return dispatch ({
+                type: 'GET_TEMPERAMENTS',
+                payload: temp.data
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export function postDog(payload) {
+    return async function(dispatch) {
+        const dogData = await axios.post('http://localhost:3001/dog', payload);
+        console.log(dogData)
+        return dogData;
+    }
+}
+
+
+export function filterDogsByTemperament(payload) {}
