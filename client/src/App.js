84f58f923c5cfa2ca1b343bbcf48ca76413d6430
Sofia <sraissiguier@gmail.com>
2021-08-15 01:50:58 -0300

    Set Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from './components/Home/Home';
+import Landing from './components/Landing/Landing';
+import DogCreate from './components/DogCreate/DogCreate';
+import DogDetail from "./components/DogDetail/DogDetail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/home" component={Home} />
+        <Route path="/create" component={DogCreate} />
+        <Route path="/detail/:id" component={DogDetail} />   
+      </Switch>
+    </>
   );
 }
 
