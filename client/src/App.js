ab4e101b32434beabbaf8c2a5ab0d7a6a7d35196
Sofia <sraissiguier@gmail.com>
2021-08-15 20:41:24 -0300

    Fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home';
 import Landing from './components/Landing/Landing';
@@ -8,14 +8,14 @@ import DogDetail from "./components/DogDetail/DogDetail";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/home" component={Home} />
         <Route path="/create" component={DogCreate} />
         <Route path="/detail/:id" component={DogDetail} />   
       </Switch>
-    </>
+    </BrowserRouter>
   );
 }
 
