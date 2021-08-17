2365cad5ded35a821937d3d31f81ceed5c7089f7
Sofia <sraissiguier@gmail.com>
2021-08-17 14:40:07 -0300

    Add style to DogDetail component

--- client/src/components/DogDetail/DogDetail.js
+++ client/src/components/DogDetail/DogDetail.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getDogDetail } from '../../actions/index';
+import style from './DogDetail.module.css';
 
 
 const DogDetail = (props) => {
@@ -17,19 +18,19 @@ const DogDetail = (props) => {
     console.log(detail)
 
     return (
-        <div>
+        <div className={style.dogstyle}>
             {detail.length > 0 ?
-                <div>
+                <div className={style.content}>
                     <h1>{detail[0].name}</h1>
-                    <img src={detail[0].image.url ? detail[0].image.url : detail[0].image} alt="" width="600px" height="380px"/>
+                    <img src={!detail[0].createdInDb ? detail[0].image.url : detail[0].image} alt=""/>
                     <p>Temperaments: {!detail[0].createdInDb ? detail[0].temperament + ', ' : detail[0].temperaments.map(el => el.name + (', '))}</p>
                     <p>Height: {!detail[0].createdInDb ? detail[0].height.metric : detail[0].height} cm</p>
                     <p>Weight: {!detail[0].createdInDb ? detail[0].weight.metric : detail[0].weight} kg</p>
                     <p>Lifespan: {!detail[0].createdInDb? detail[0].life_span : detail[0].life_span + (' years')}</p>
                 </div>    
-        : <p>Loading...</p>}
+                : <p>Loading...</p>}
             <Link to='/home'>
-                <button>Go Back</button>
+                <button className={style.button}>Go Back</button>
             </Link>
         </div>
     )
