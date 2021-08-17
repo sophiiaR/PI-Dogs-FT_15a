da71c28dd0bd03ce5c754a0ab61ef3c2c17030bb
Sofia <sraissiguier@gmail.com>
2021-08-17 12:19:16 -0300

    Modify landing h1 and p

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from './Landing.module.css';
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Welcome to Dog's Blog</h1>
-            <h2>The world just got better for Dog Lovers! The latest Dogs Pedia – Dog Breed Identifier is the perfect page for anyone who just can’t resist these four-legged angels.
-            From breed identification to breed selection, this Dog Identifier app is your very own mini-encyclopedia about everything related to dogs.</h2>
-            <Link to = '/home'>
-                <button>Let's go</button>
-            </Link>
+        <div className={style.home}>
+                <div className={style.content}>
+                    <h1>Dogspedia</h1>
+                    <p>The latest Dogs Pedia – Dog Breed Identifier</p>
+                    <Link to = '/home'>
+                        <button>Let's go</button>
+                    </Link>
+                </div>       
         </div>
     )
 }
 
 export default Landing;
+
+
