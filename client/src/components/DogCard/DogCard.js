cb8a69b4a32539e38f66f6fa023a366bda320e75
Sofia <sraissiguier@gmail.com>
2021-08-18 03:12:22 -0300

    Add style to DogCard component

--- client/src/components/DogCard/DogCard.js
+++ client/src/components/DogCard/DogCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './DogCard.module.css';
 
 const DogCard = ({ name, image, temperament }) => {
     return (
-        <div>
-            <p>{name}</p>
-            <img src={image} alt="dogimg" width="200px" height="200px" />
+        <div className={style.content}>
+            <p className={style.title}>{name}</p>
+            <img src={image} alt="dogimg" width="200px" height="200px"/>
             <p>{temperament}</p>
         </div>
     )
